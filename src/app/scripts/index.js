411dc374657341fc20a94d062f077b1691075b0e
SOFIA VITERY <jsofiavm@gmail.com>
2024-03-06 19:34:48 -0500

    Actualizacion rutas images local y estilos logo

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -1,11 +1,15 @@
 import "../styles/style.scss";
+import image from "../assets/images/LOGO.png";
+
+const logoImage = document.getElementById("logo");
+logoImage.setAttribute("src", image);
 
 // //Declaración de variables y constantes
 
 
 
 function toggleLogin() {
-    document.getElementById("login-toggle").style.backgroundColor = "#57B846";
+    document.getElementById("login-toggle").style.backgroundColor = "#511684";
     document.getElementById("login-toggle").style.color = "#fff";
     document.getElementById("signup-toggle").style.backgroundColor = "#fff";
     document.getElementById("signup-toggle").style.color = "#222";
@@ -15,7 +19,7 @@ function toggleLogin() {
 function toggleSignup() {
     document.getElementById("login-toggle").style.backgroundColor = "#fff";
     document.getElementById("login-toggle").style.color = "#222";
-    document.getElementById("signup-toggle").style.backgroundColor = "#57b846";
+    document.getElementById("signup-toggle").style.backgroundColor = "#511684";
     document.getElementById("signup-toggle").style.color = "#fff";
     document.getElementById("login-form").style.display = "none";
     document.getElementById("signup-form").style.display = "block";
